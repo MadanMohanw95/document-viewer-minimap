28a43239a418c3afce506ecc06703ef249166975
mosnegi <metudoreanu@ualr.edu>
2022-12-12 14:55:47 -0800

    changed + and - to arrows; added hover css

--- src/components/SideBySide.js
+++ src/components/SideBySide.js
@@ -9,16 +9,17 @@ const SideBySide = (props)=>{
 
     return (
     <div id='diff-element-content'>
-        <span
+        <span id='clickable'
             onClick={() => setCollapsed(!isCollapsed)}
             style={{backgroundColor: (leftSide !== rightSide ? 'lightgray': '')}}
         >
-                {props.paragraphIndex.toString().padStart(3, "0")}{isCollapsed ? '+' : '-'} </span>
+                {props.paragraphIndex.toString().padStart(3, "0")}{(isCollapsed ? '>' : '\u2304')} 
+        </span>
         <div id='diff-element-left'>           
-            <Markup content={isCollapsed ? leftSide.split(' ').slice(0, 5).join(' ') : leftSide} />
+            <Markup content={isCollapsed ? leftSide.split(' ').slice(0, 5).join(' ')+'.....' : leftSide} />
         </div>
         <div id='diff-element-right'>
-            <Markup content={isCollapsed ? rightSide.split(' ').slice(0, 5).join(' ') : rightSide} />
+            <Markup content={isCollapsed ? rightSide.split(' ').slice(0, 5).join(' ')+'.....' : rightSide} />
         </div>
     </div>
 
